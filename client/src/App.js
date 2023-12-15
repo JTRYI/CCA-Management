8ea05591f502a4036891ec7d0ef094747ab1806c
Jivaka <teijivaka@gmail.com>
2023-12-15 20:59:42 +0800

    Set Up Client Side Folder

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1 className="text-6xl">
+        Hello World
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
