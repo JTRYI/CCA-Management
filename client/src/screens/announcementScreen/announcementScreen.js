0b48e5ccdb5f5dd6f310b2e2c570d809856baed7
Jivaka <teijivaka@gmail.com>
2024-01-06 22:49:26 +0800

    Delete Announcement Frontend Implemented

--- client/src/screens/announcementScreen/announcementScreen.js
+++ client/src/screens/announcementScreen/announcementScreen.js
@@ -1,22 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import './announcementScreen.css';
-import { Button, useDisclosure } from '@chakra-ui/react';
+import { Button, useDisclosure, useToast } from '@chakra-ui/react';
 import { ImPencil } from "react-icons/im";
 import { ImBin } from "react-icons/im";
 import AddAnnouncementModal from '../../components/addAnnouncementModal/addAnnouncementModal';
 import EditAnnouncementModal from '../../components/editAnnouncementModal/editAnnouncementModal'
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+} from '@chakra-ui/react'
 
 const Announcement = (props) => {
 
   const editAnnouncementDisclosure = useDisclosure();
   const [editAnnouncementClicked, setEditAnnouncementClicked] = useState(false);
 
+  const deleteAnnouncementDisclosure = useDisclosure();
+
+  const user = JSON.parse(sessionStorage.getItem('user'));
+  
   return (
     <div className='announcement-block' style={{ width: '100%', backgroundColor: 'white', height: 'auto', borderRadius: '15px', marginBottom: '20px' }}>
       <div className='padding-box' style={{ display: 'flex', flexDirection: 'column', padding: '20px', position: 'relative' }}>
         <h1 style={{ paddingBottom: '10px', fontSize: '18px', fontWeight: 'bold' }}>{props.announcement.title}</h1>
         <div className='edi-del-icons' style={{ display: 'flex', position: 'absolute', top: '20px', right: '20px' }}>
-          <ImPencil style={{ marginRight: '15px', color: '#996515', cursor: 'pointer' }} onClick={() => {
+          <ImPencil style={{ marginRight: '15px', color: '#996515', cursor: 'pointer' , display: user.isAdmin ? 'block' : 'none' }} onClick={() => {
             setEditAnnouncementClicked(true);
             editAnnouncementDisclosure.onOpen();
           }} />
@@ -29,7 +41,35 @@ const Announcement = (props) => {
               announcementID={props.announcement._id}
               afterCloseCallback={props.handleModalClose} />
           )}
-          <ImBin style={{ color: 'red' }} />
+          <ImBin style={{ color: 'red', cursor: 'pointer', display: user.isAdmin ? 'block' : 'none' }} onClick={deleteAnnouncementDisclosure.onOpen} />
+          <AlertDialog
+              isOpen={deleteAnnouncementDisclosure.isOpen}
+              onClose={deleteAnnouncementDisclosure.onClose}
+            >
+              <AlertDialogOverlay>
+                <AlertDialogContent>
+                  <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+                    {`Delete Announcement`}
+                  </AlertDialogHeader>
+
+                  <AlertDialogBody>
+                    Are you sure? You can't undo this action afterwards.
+                  </AlertDialogBody>
+                  <AlertDialogFooter>
+                    <Button onClick={deleteAnnouncementDisclosure.onClose}>
+                      Cancel
+                    </Button>
+                    <Button colorScheme='red'
+                      onClick={() => {
+                        props.deleteAnnouncement(props.announcement._id);
+                        deleteAnnouncementDisclosure.onClose();
+                      }} ml={3}>
+                      Delete
+                    </Button>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialogOverlay>
+            </AlertDialog>
         </div>
         <p>{props.announcement.description}</p>
       </div>
@@ -41,10 +81,12 @@ const Announcement = (props) => {
 function AnnouncementScreen() {
 
   const user = JSON.parse(sessionStorage.getItem('user'));
+  const token = sessionStorage.getItem('token');
 
   const [announcements, setAnnouncement] = useState([]);
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [updateTrigger, setUpdateTrigger] = useState(false);
+  const toast = useToast();
 
   const handleModalClose = () => {
     // Toggle the updateTrigger to trigger re-render
@@ -77,8 +119,29 @@ function AnnouncementScreen() {
           announcement={announcement}
           key={announcement._id}
           handleModalClose={handleModalClose}
+          deleteAnnouncement = {() => {
+            deleteAnnouncement(announcement._id)
+          }}
         />
       );
+    })
+  }
+
+  //This method will delete a Announcement
+  async function deleteAnnouncement(id) {
+    await fetch(`http://localhost:5050/announcements/${token}/${id}`, {
+      method: "DELETE"
+    });
+
+    const newAnnouncements = announcements.filter((el) => el._id !== id);
+    setAnnouncement(newAnnouncements);
+
+    toast({
+      title: 'Request Successful',
+      description: "Announcement Deleted Successfully!",
+      status: 'success',
+      duration: 5000,
+      isClosable: true,
     })
   }
 
