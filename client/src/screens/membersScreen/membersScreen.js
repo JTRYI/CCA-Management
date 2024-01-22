dd748b2dd69241234b874745f42047ec23d04e54
Jivaka <teijivaka@gmail.com>
2024-01-22 20:07:21 +0800

    Added Filtering and Sorting of Members Feature

--- client/src/screens/membersScreen/membersScreen.js
+++ client/src/screens/membersScreen/membersScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './membersScreen.css';
-import { Button, Input, InputGroup, InputRightElement, Table, TableContainer, Td, Tr, Thead, Th, Tbody, Avatar, Menu, MenuList, MenuItem, MenuButton, IconButton, Icon } from '@chakra-ui/react';
+import { Button, Input, InputGroup, InputRightElement, Table, TableContainer, Td, Tr, Thead, Th, Tbody, Avatar, Menu, MenuList, MenuItem, MenuButton, IconButton, Icon, MenuItemOption, MenuOptionGroup, Box } from '@chakra-ui/react';
 import { useState, useEffect } from 'react';
 import { useDisclosure, useToast } from '@chakra-ui/react';
 import AddModal from '../../components/addModal/addModal';
@@ -8,6 +8,11 @@ import { FiMoreVertical } from 'react-icons/fi';
 import { FaRegEye } from "react-icons/fa";
 import { ImPencil } from "react-icons/im";
 import { ImBin } from "react-icons/im";
+import { BsFunnel } from "react-icons/bs";
+import { FaSortAlphaUp } from "react-icons/fa";
+import { FaSortNumericDownAlt } from "react-icons/fa";
+import { FcClearFilters } from "react-icons/fc";
+
 import EditModal from '../../components/editModal/editModal';
 import {
   AlertDialog,
@@ -27,13 +32,13 @@ const Member = (props) => {
   const [viewProfileClicked, setViewProfileClicked] = useState(false);
   const [editProfileClicked, setEditProfileClicked] = useState(false);
   const user = JSON.parse(sessionStorage.getItem('user'));
-  
+
   return (
     <Tr>
       <Td>
         <div className='name-profile'>
           <Avatar size='md' bg='yellow.400' name={props.member.profilePic == null ? props.member.name : null}  // Display name only if profilePic is null
-                src={props.member.profilePic} style={{ marginRight: "10%" }} />
+            src={props.member.profilePic} style={{ marginRight: "10%" }} />
           {props.member.name}
         </div>
       </Td>
@@ -50,23 +55,23 @@ const Member = (props) => {
             <MenuItem icon={<FaRegEye />} onClick={() => {
               setViewProfileClicked(true);
               viewProfileDisclosure.onOpen();
-            } }>
+            }}>
               View Profile
             </MenuItem>
             {viewProfileClicked && (
-              <ViewModal isOpen={viewProfileDisclosure.isOpen} 
-              onClose={ () => {
-                setViewProfileClicked(false);
-                viewProfileDisclosure.onClose();
-              }} 
-              memberID={props.member._id}/>
-              )}
+              <ViewModal isOpen={viewProfileDisclosure.isOpen}
+                onClose={() => {
+                  setViewProfileClicked(false);
+                  viewProfileDisclosure.onClose();
+                }}
+                memberID={props.member._id} />
+            )}
 
             <MenuItem icon={<ImPencil />} onClick={() => {
               setEditProfileClicked(true);
               editProfileDisclosure.onOpen();
             }}
-            style={{ display: user.isAdmin ? 'block' : 'none' }}>
+              style={{ display: user.isAdmin ? 'block' : 'none' }}>
               Edit Profile
             </MenuItem>
             {editProfileClicked && (
@@ -122,6 +127,16 @@ const Member = (props) => {
 function MembersScreen() {
 
   const [members, setMembers] = useState([]);
+
+  const [selectedFilter, setSelectedFilter] = useState('all');
+  const [filteredMembers, setFilteredMembers] = useState(members);
+
+  const [sortOrder, setSortOrder] = useState('asc');
+  const [sortColumn, setSortColumn] = useState(null);
+  const [isNameSorted, setIsNameSorted] = useState(false);
+  const [isInstrumentSorted, setIsInstrumentSorted] = useState(false);
+  const [isDateJoinedSorted, setIsDateJoinedSorted] = useState(false);
+
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [updateTrigger, setUpdateTrigger] = useState(false);
   const toast = useToast();
@@ -143,14 +158,14 @@ function MembersScreen() {
       }
 
       const members = await response.json();
-      setMembers(members)
+      setMembers(members);
+      setSelectedFilter('all');
     }
 
     getMembers();
 
     return;
   }, [updateTrigger]);
-
 
   const token = sessionStorage.getItem('token');
 
@@ -172,21 +187,105 @@ function MembersScreen() {
     })
   }
 
-  //This method will map out the members on the table
-  function memberList() {
-    return members.map((member) => {
-      return (
-        <Member
-          member={member}
-          key={member._id}
-          handleModalClose={handleModalClose}
-          deleteMember={() => {
-            deleteMember(member._id);
-          }}
-        />
-      );
-    });
+  // Function to convert date string to a sortable value
+  const getSortableDateValue = (dateString) => {
+    const [day, month, year] = dateString.split('-').map(Number);
+    return year * 10000 + month * 100 + day;
+  };
+
+  // This method will handle sorting based on the selected column
+  const handleSort = (column) => {
+    // Toggle the sort order if the same column is clicked
+    const newSortOrder = sortColumn === column ? (sortOrder === 'asc' ? 'desc' : 'asc') : 'asc';
+    setSortOrder(newSortOrder);
+    setSortColumn(column);
+
+    // Sort the members array based on the selected column and order
+    let sortedMembers = [...filteredMembers];
+    switch (column) {
+
+      case 'name':
+        sortedMembers.sort((a, b) => {
+          const comparison = a[column].localeCompare(b[column]);
+          return sortOrder === 'asc' ? comparison : -comparison;
+        });
+        setIsNameSorted(true);
+        setIsInstrumentSorted(false);
+        setIsDateJoinedSorted(false);
+        break;
+
+      case 'instrument':
+        sortedMembers.sort((a, b) => {
+          const comparison = a[column].localeCompare(b[column]);
+          return sortOrder === 'asc' ? comparison : -comparison;
+        });
+        setIsNameSorted(false);
+        setIsInstrumentSorted(true);
+        setIsDateJoinedSorted(false);
+        break;
+
+      case 'dateJoined':
+        sortedMembers.sort((a, b) => {
+          const valueA = getSortableDateValue(a[column]);
+          const valueB = getSortableDateValue(b[column]);
+
+          return sortOrder === 'asc' ? valueA - valueB : valueB - valueA;
+        });
+
+        setIsNameSorted(false);
+        setIsInstrumentSorted(false);
+        setIsDateJoinedSorted(true);
+
+        break;
+      default:
+        break;
+    }
+
+    setFilteredMembers(sortedMembers);
+  };
+
+  // Function to handle canceling the sort/filter and return to the original order
+  const handleCancelSortFilter = () => {
+    setFilteredMembers([...members]); // Restore the original order
+    setSelectedFilter('all');
+    setIsNameSorted(false);
+    setIsInstrumentSorted(false);
+    setIsDateJoinedSorted(false);
+  };
+
+
+
+  useEffect(() => {
+    // This effect will run whenever selectedFilter or members change
+    let updatedFilteredMembers = members;
+
+    if (selectedFilter !== 'all') {
+      // Apply filtering based on the selected year
+      updatedFilteredMembers = members.filter((member) => member.yearOfStudy === selectedFilter);
+    }
+
+    setFilteredMembers(updatedFilteredMembers);
+    setIsNameSorted(false);
+    setIsInstrumentSorted(false);
+    setIsDateJoinedSorted(false);
+
+  }, [selectedFilter, members]); // Dependency array to ensure this effect runs when these values change
+
+  // This method will map out the members on the table based on the selected filter
+  function filteredMemberList() {
+
+    return filteredMembers.map((member) => (
+      <Member
+        member={member}
+        key={member._id}
+        handleModalClose={handleModalClose}
+        deleteMember={() => {
+          deleteMember(member._id);
+        }}
+      />
+    ));
   }
+
 
   const user = JSON.parse(sessionStorage.getItem('user'));
 
@@ -207,7 +306,7 @@ function MembersScreen() {
               }
             }
 
-            
+
           />
           <InputRightElement>
             <img src='icons/magnifying-glass-solid.svg'></img>
@@ -216,8 +315,25 @@ function MembersScreen() {
 
         <div className='top-header'>
           <div className='left-side-headers'>
-            <h1>{members.length} Members</h1>
-            <img src='icons/arrow-down-wide-short-solid.svg' />
+            <h1>{filteredMembers.length} Members</h1>
+            <Box key={selectedFilter} style={{ zIndex: 2 }}>
+              <Menu>
+                <MenuButton as={IconButton} variant='' icon={<BsFunnel />} style={{ fontSize: '20px', paddingLeft: '10%', marginTop: '2px' }} />
+                <MenuList>
+                  <MenuOptionGroup defaultValue={selectedFilter} title='Filters:' type='radio' onChange={(value) => setSelectedFilter(value)}>
+                    <MenuItemOption value='all'>All Members</MenuItemOption>
+                    <MenuItemOption value="1">Year 1</MenuItemOption>
+                    <MenuItemOption value="2">Year 2</MenuItemOption>
+                    <MenuItemOption value="3">Year 3</MenuItemOption>
+                  </MenuOptionGroup>
+                </MenuList>
+              </Menu>
+            </Box>
+
+            <Button rightIcon={<FcClearFilters />} colorScheme='orange' variant='ghost' height={8} margin={1} onClick={handleCancelSortFilter}>
+            Clear
+          </Button>
+
           </div>
           <Button className='right-side-header' backgroundColor='rgba(153, 101, 21, 0.5);'
             color={'white'} borderRadius={10} paddingLeft={8} paddingRight={7} height={8}
@@ -230,7 +346,7 @@ function MembersScreen() {
             onClick={() => {
               onOpen();
             }}
-            
+
             style={{ visibility: user.isAdmin ? 'visible' : 'hidden' }}
 
           >Add
@@ -243,13 +359,13 @@ function MembersScreen() {
           <Table className='members-table' variant='striped' colorScheme='orange' size='sm'>
             <Thead style={{ position: 'sticky', top: 0, backgroundColor: 'white', zIndex: 1 }}>
               <Tr>
-                <Th paddingY={5} paddingStart={20} color='#996515'>Name</Th>
-                <Th color='#996515'>Instrument</Th>
-                <Th color='#996515'>Date Joined</Th>
+                <Th paddingY={5} paddingStart={20} color='#996515' style={{ transform: 'translateY(7px)' }} >Name <FaSortAlphaUp style={{ fontSize: '14px', transform: 'translateY(-15px) translateX(45px)', cursor: 'pointer', color: isNameSorted ? '#996515' : 'black' }} onClick={() => handleSort('name')} /></Th>
+                <Th color='#996515' style={{ transform: 'translateY(7px)' }}>Instrument <FaSortAlphaUp style={{ fontSize: '14px', transform: 'translateY(-15px) translateX(90px)', cursor: 'pointer', color: isInstrumentSorted ? '#996515' : 'black' }} onClick={() => handleSort('instrument')} /></Th>
+                <Th color='#996515' style={{ transform: 'translateY(7px)' }}>Date Joined <FaSortNumericDownAlt style={{ fontSize: '14px', transform: 'translateY(-15px) translateX(88px)', cursor: 'pointer', color: isDateJoinedSorted ? '#996515' : 'black' }} onClick={() => handleSort('dateJoined')} /></Th>
                 <Th color='#996515'>Actions</Th>
               </Tr>
             </Thead>
-            <Tbody>{memberList()}</Tbody>
+            <Tbody>{filteredMemberList()}</Tbody>
           </Table>
         </TableContainer>
       </div>
